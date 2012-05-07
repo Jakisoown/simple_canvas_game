144643960a2a6a76999991f2a479feb6bcf4822e
Jakisoown <Jakisoown@aim.com>
2012-05-07 15:17:44 -0700

    Update js/game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 1000;
-canvas.height = 1000;
+canvas.width = 512;
+canvas.height = 480;
 document.body.appendChild(canvas);
 
 // Background image
